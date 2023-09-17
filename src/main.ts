e8d26881a2e597df13991f7eb2e162bc7d0ae4fc
xuchenggang <chenggang.xu@xipu.com>
2023-09-17 10:50:22 +0800

    feat: env

--- src/main.ts
+++ src/main.ts
@@ -8,8 +8,8 @@ import zhCn from 'element-plus/dist/locale/zh-cn.mjs'
 
 const app = createApp(App)
 
-app.use(ElementPlus,{
-    locale: zhCn,
+app.use(ElementPlus, {
+  locale: zhCn,
 })
 
 app.mount('#app')
